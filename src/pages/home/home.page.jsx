ac25a99bc47cdf132d0ee08b09fbb488d777d736
Saverio683 <saveriorandazzo50@gmail.com>
2022-06-18 16:13:07 +0200

    homepage, pagina dei servizi e la sezione luce e gas

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,77 +1,73 @@
-import { useState } from 'react'
-import './home.styles.scss'
-import Buttoncontatta from '../../components/buttoncontatta/Buttoncontatta'
-import Footer from '../../components/footer/footer.component'
+import { useEffect, useState, useRef } from 'react'
 
+import './home.styles.scss'
+
+const galleriaDiImmagini = [
+    'https://i.imgur.com/Bvjfcjy.png',
+    'https://i.imgur.com/b4mfMq9.jpeg',
+    'https://cdn.wallpapersafari.com/22/23/E3vNtI.jpg',
+    'https://wallpaperaccess.com/full/343619.jpg',
+    'https://cdn.gobankingrates.com/wp-content/uploads/2019/05/Davao-City-in-the-Phillippines-shutterstock_504042466.jpg'
+]
+
+const delay = 4000
 
 const HomePage = () => {
-  //state per far apparire il contattaci dopo il click
-  const [cont, setCont]=useState(false);
-  return (
-<div className='homepage'> 
+    const [sfondo, setSfondo] = useState(galleriaDiImmagini[0])
+    const [index, setIndex] = useState(0)
+    const timeoutRef = useRef(null)
+    const [movimento, setMovimento] = useState(true)
 
- {/* slide di immagini */}
- <div className="container">
-    {console.log(cont)}
-     <input type="radio" id="i1" name="images" defaultChecked />
-     <input type="radio" id="i2" name="images" />
-     <input type="radio" id="i3" name="images" />
-     <input type="radio" id="i4" name="images" />
-     <input type="radio" id="i5" name="images" />    
-     
-     {/* immagine 1 */}
-     <div className="slide_img" id="one">
-         {/* bottone contattaci davanti immagine */}
-         <Buttoncontatta/>            
-             <img src="http://www.bhmpics.com/wallpapers/little_pony_art-800x480.jpg" />
-             
-                 <label className="prev" htmlFor="i5"><span>&#x2039;</span></label>
-                 <label className="next" htmlFor="i2"><span>&#x203a;</span></label>       
-     </div>
-     {/* immagine 2 */}
-     <div className="slide_img" id="two">
-     <Buttoncontatta/> 
-             <img src="https://preview.ibb.co/e5OShF/cropped_800_480_111290.jpg " />
-             
-                 <label className="prev" htmlFor="i1"><span>&#x2039;</span></label>
-                 <label className="next" htmlFor="i3"><span>&#x203a;</span></label>
-         
-     </div>
-     {/* immagine 3 */}     
-     <div className="slide_img" id="three">
-     <Buttoncontatta/> 
-             <img src="http://wallpaperswide.com/download/up_house-wallpaper-1280x800.jpg" />    
-             
-                 <label className="prev" htmlFor="i2"><span>&#x2039;</span></label>
-                 <label className="next" htmlFor="i4"><span>&#x203a;</span></label>
-     </div>
-     {/* immagine 4 */}
-     <div className="slide_img" id="four">
-     <Buttoncontatta/> 
-             <img src="http://wallpaperswide.com/download/big_hero_6_baymax-wallpaper-1152x720.jpg" />    
-             
-                 <label className="prev" htmlFor="i3"><span>&#x2039;</span></label>
-                 <label className="next" htmlFor="i5"><span>&#x203a;</span></label>
-     </div>
-     {/* immagine 5 */}
-     <div className="slide_img" id="five">
-     <Buttoncontatta/> 
-             <img src="https://cdn.gobankingrates.com/wp-content/uploads/2019/05/Davao-City-in-the-Phillippines-shutterstock_504042466.jpg"/>    
-               
-                 <label className="next" htmlFor="i1"><span>&#x203a;</span></label>
-     </div>
-     <div id="nav_slide">
-         <label htmlFor="i1" className="dots" id="dot1"></label>
-         <label htmlFor="i2" className="dots" id="dot2"></label>
-         <label htmlFor="i3" className="dots" id="dot3"></label>
-         <label htmlFor="i4" className="dots" id="dot4"></label>
-         <label htmlFor="i5" className="dots" id="dot5"></label>
-     </div>  
-    
- </div> 
- <Footer/> 
-</div>
-  )
+    const resetTimeout = () => timeoutRef.current ? clearTimeout(timeoutRef.current) : null
+
+    useEffect(() => {
+        resetTimeout()
+        timeoutRef.current = setTimeout(
+            () => 
+                setIndex(prevIndex =>
+                    prevIndex === galleriaDiImmagini.length - 1 ? 0 : prevIndex + 1
+                ),
+            delay
+        )
+      
+        return () => {
+            resetTimeout()
+        }
+    }, [index, movimento]) 
+
+    return (
+        <div className='homepage'> 
+            <div 
+                className='slide-container'
+                style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+            >
+                {
+                    galleriaDiImmagini.map((img, i) => (
+                        <div 
+                            key={i}
+                            className='slide'
+                            style={{
+                                'background': `url(${img}) no-repeat center center fixed`
+                            }}                            
+                        >
+                        </div>
+                    ))
+            }
+            </div>
+            <div className="punti-container">
+                {galleriaDiImmagini.map((_, i) => (
+                    <div
+                        key={i}
+                        className={`punto${index === i ? " active" : ""}`}
+                        onClick={() => {
+                            setIndex(i);
+                        }}
+                    >
+                    </div>
+                ))}
+            </div>          
+        </div>
+    )
 }
 
 export default HomePage
